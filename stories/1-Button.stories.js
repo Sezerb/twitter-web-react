bb675cb4f18b05cd3d4d9fcaff0c47bd388e2132
sezer.baglan <sezerbaglan@gmail.com>
2022-01-25 00:11:28 +0300

    add theme button

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -4,12 +4,23 @@ import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
 import Navigation from '../components/navigation'
 import { Home } from '../components/icons'
+import ThemeButton from '../components/theme-button'
+import Stack from '../components/stack'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>Save</Button>
+export const Theme = () => (
+  <Stack column gap={20}>
+    <ThemeButton>Save</ThemeButton>
+    <ThemeButton full>Save Full</ThemeButton>
+    <ThemeButton full big>
+      Save Big Button
+    </ThemeButton>
+  </Stack>
+)
 
 export const NavButton = () => (
   <NavigationButton>
